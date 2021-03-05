e8d14d1778b335dbca8ebeab4d3acbea88fd5295
Nhaster <mrbroume@gmail.com>
2021-03-05 21:12:58 +0300

    dead code

--- src/exchange/constants.ts
+++
@@ -1,1 +0,0 @@
-export const BITCOIN_LAUNCH = Date.UTC(2009, 0, 1);
--- src/exchange/exchange.utils.ts
+++
@@ -1,3 +0,0 @@
-export const getTableName = (symbol: string, timeframe: string) => {
-  return `${symbol.toLowerCase().replace('/', '_')}_${timeframe}`;
-};
